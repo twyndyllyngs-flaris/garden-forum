9c62c68107d008a1bb3a4df2c64419ebe8d1da3f
Ralph Matthew De Leon <rmdldeleon@gmail.com>
2024-10-26 02:03:09 +0800

    added scrollbar style for the entire project

--- src/pages/guides.tsx
+++ src/pages/guides.tsx
@@ -5,6 +5,8 @@ import '../styling/output.css';
 //components
 import GuidesSidebar from "../components/sidebar/guides-sidebar";
 import GuidesMain from "../components/main/guides-main";
+import { ScrollArea } from "../components/ui/scroll-area"
+
 
 function Guides() {  return (
     <div className="flex flex-1 max-w-full overflow-auto">
